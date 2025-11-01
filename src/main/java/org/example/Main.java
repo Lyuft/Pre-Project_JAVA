824c3fc0720edc6c9b4bb1600c50e81ae184f07d
Nikita <nik.tsubulay@mail.ru>
2025-11-02 02:23:45 +0300

    test commit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello JAVA!!!!");
+        int[] array = new int[10];
+        for (int i : array){
+            System.out.println(i);
+        }
     }
 }
